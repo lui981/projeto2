0faae3863dd58233995fabe1064207630607a2f2
aluno <aluno@ubunturails.myguest.virtualbox.org>
2023-04-25 10:19:36 -0300

    Criacao dos Usuarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    layout :layout
+    def layout
+        if current_user != nil
+            @layout ="application"
+        else
+            @layout ="applicationlogin"
+        end
+    end  
 end
