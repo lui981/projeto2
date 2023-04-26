b2b2506c15435534532f43334028b2432efae80f
aluno <aluno@ubunturails.myguest.virtualbox.org>
2023-04-26 10:42:50 -0300

    parte do login

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   resources :produtos
   resources :fornecedors
   root "produtos#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(nome: "domingas", telefone: "212121455", email: "durval@gmail.com", password: "123456")
+User.create(nome: "duda", telefone: "2121214999", email: "dudas@gmail.com", password: "123456")
+
