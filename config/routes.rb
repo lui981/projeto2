c5286ad155783127251f799b68426a76627c43cd
aluno <aluno@ubunturails.myguest.virtualbox.org>
2023-04-27 08:58:14 -0300

    atualizacao do projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :produtos
+  resources :produtos, :path => "produtos" do
+    collection do
+      get "/relatorio"  => "produtos#relatorio", :as => "relatorio" 
+    end
+  end
   resources :fornecedors
   root "produtos#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@
 User.create(nome: "domingas", telefone: "212121455", email: "durval@gmail.com", password: "123456")
 User.create(nome: "duda", telefone: "2121214999", email: "dudas@gmail.com", password: "123456")
 
+clear
